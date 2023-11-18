29703b7c13bde438583c5bb829551c61ce99398a
mancarius <mattia.mancarella.dev@gmail.com>
2023-11-18 18:42:02 +0100

    create Fruit and Nutritions types

---
+++ src/app/shared/types/fruit.ts
@@ -0,0 +1,10 @@
+import { Nutritions } from "./nutritions"
+
+export type Fruit = {
+  genus: string,
+  name: string,
+  id: number,
+  family: string,
+  order: string,
+  nutritions: Nutritions
+}
