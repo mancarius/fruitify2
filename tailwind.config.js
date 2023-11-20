2586dad54419f0aa5153c566b68d32c012d5c8f2
mancarius <mattia.mancarella.dev@gmail.com>
2023-11-20 15:03:44 +0000

    configure initial environment with tailwind and angular-material

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
+  important: true,
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
 
