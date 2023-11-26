14b5e18fe82e0e5b59e60ba6d7e485466b67bce1
mancarius <mattia.mancarella.dev@gmail.com>
2023-11-26 16:14:29 +0000

    Add match property to ApiAuthConfig

--- src/app/shared/types/api-auth-config.ts
+++ src/app/shared/types/api-auth-config.ts
@@ -1,9 +1,11 @@
 export type ApiAuthConfig = {
+  match: string;
   addTo: "headers";
   key: string,
   authorizationType: string;
   value: string;
 } | {
+  match: string;
   addTo: "params";
   key: string;
   authorizationType: never;
