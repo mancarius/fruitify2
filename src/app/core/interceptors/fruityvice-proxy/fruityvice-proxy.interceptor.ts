4a5e91b5fe9381af28ef291c790b30d95af7782d
mancarius <mattia.mancarella.dev@gmail.com>
2023-11-26 16:15:20 +0000

    Update Fruityvice proxy interceptor to use new base URL

--- src/app/core/interceptors/fruityvice-proxy/fruityvice-proxy.interceptor.ts
+++ src/app/core/interceptors/fruityvice-proxy/fruityvice-proxy.interceptor.ts
@@ -1,22 +1,21 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import { API_BASE_URL } from '@shared/constants';
-import { environment } from '@env/environment.development';
+import { API_BASE_PATHNAME, FRUITYVICE_PROXY_BASE_URL } from '@shared/constants';
 
 /**
- * Interceptor for modifying the request URL to match the Fruityvice API base URL.
+ * Interceptor function that modifies the request URL to use a proxy server for requests matching the base URL.
  * @param req - The original HTTP request.
  * @param next - The next interceptor in the chain.
  * @returns The modified HTTP request.
  */
 export const fruityviceProxyInterceptor: HttpInterceptorFn = (req, next) => {
   const url = new URL(req.url);
-  const matchesBaseUrl = url.pathname.includes(API_BASE_URL);
+  const matchesBaseUrl = url.pathname.includes(API_BASE_PATHNAME);
 
   if (!matchesBaseUrl) {
     return next(req);
   }
 
-  url.hostname = environment.api;
+  url.hostname = FRUITYVICE_PROXY_BASE_URL;
   url.protocol = 'https';
   url.port = '';
 
