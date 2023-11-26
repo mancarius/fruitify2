b6bf58f7adf6d352d3be868e2cb4332a62d1efcf
mancarius <mattia.mancarella.dev@gmail.com>
2023-11-26 15:50:45 +0000

    Refactor app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,22 +6,38 @@ import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { fruityviceProxyInterceptor } from '@core/interceptors/fruityvice-proxy/fruityvice-proxy.interceptor';
+import { BehaviorSubject } from 'rxjs';
+import { ApiAuthConfig } from '@shared/types/api-auth-config';
+import { API_AUTH_CONFIG } from '@tokens';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideAnimations(),
-    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
-    provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    }),
-    provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    }),
-    provideHttpClient(
-        withInterceptors([fruityviceProxyInterceptor])
-    )
-]
+    providers: [
+        provideRouter(routes),
+
+        provideAnimations(),
+
+        provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+        }),
+
+        provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+        }),
+
+        provideHttpClient(
+            withInterceptors([fruityviceProxyInterceptor])
+        ),
+
+        {
+            provide: API_AUTH_CONFIG,
+            useFactory: () => new BehaviorSubject<ApiAuthConfig|null>(null)
+        },
+
+        {
+            provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+            useValue: { appearance: 'outline' }
+        },
+    ]
 };
