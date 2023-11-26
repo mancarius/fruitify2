66682832c404754b5b78549288e2426608d5364b
mancarius <mattia.mancarella.dev@gmail.com>
2023-11-26 15:29:29 +0000

    Update API constants and authentication configurations

--- src/app/shared/constants/index.ts
+++ src/app/shared/constants/index.ts
@@ -1,3 +1,26 @@
-export const API_BASE_URL = '/api/fruit';
+import { environment } from "@env/environment.development";
+import { ApiAuthConfig } from "@shared/types/api-auth-config";
 
-export const PEXELS_API_BASE_URL = 'https://api.pexels.com';
+export const API_BASE_PATHNAME = '/api/fruit';
+
+export const FRUITYVICE_PROXY_BASE_URL = environment.fruityviceProxy;
+
+export const PEXELS_API_BASE_URL = environment.pexelsApi;
+
+export const PEXELS_API_AUTH_CONFIG: ApiAuthConfig = {
+  addTo: "headers",
+  key: "Authorization",
+  authorizationType: "",
+  value: environment.pexelsApiKey
+};
+
+export const PEXELS_API_AUTH_KEY = "Authorization";
+
+export const UNSPLASH_API_BASE_URL = environment.unsplashApi;
+
+export const UNSPLASH_API_AUTH_CONFIG: ApiAuthConfig = {
+  addTo: "headers",
+  key: "Authorization",
+  authorizationType: "Client-ID",
+  value: `${environment.unsplashApiKey}`
+};
