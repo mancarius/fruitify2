4543f957728234a9fe4540821ad4aff0d1c78060
mancarius <mattia.mancarella.dev@gmail.com>
2023-11-26 16:14:44 +0000

    Fix API authentication configuration

--- src/app/shared/constants/index.ts
+++ src/app/shared/constants/index.ts
@@ -8,17 +8,17 @@ export const FRUITYVICE_PROXY_BASE_URL = environment.fruityviceProxy;
 export const PEXELS_API_BASE_URL = environment.pexelsApi;
 
 export const PEXELS_API_AUTH_CONFIG: ApiAuthConfig = {
+  match: PEXELS_API_BASE_URL,
   addTo: "headers",
   key: "Authorization",
   authorizationType: "",
   value: environment.pexelsApiKey
 };
 
-export const PEXELS_API_AUTH_KEY = "Authorization";
-
 export const UNSPLASH_API_BASE_URL = environment.unsplashApi;
 
 export const UNSPLASH_API_AUTH_CONFIG: ApiAuthConfig = {
+  match: UNSPLASH_API_BASE_URL,
   addTo: "headers",
   key: "Authorization",
   authorizationType: "Client-ID",
