b2787aa0c64172abb3b5579016695f2731570e50
mancarius <mattia.mancarella.dev@gmail.com>
2023-11-30 17:02:30 +0000

    Fix API authentication interceptor

--- src/app/core/interceptors/api-auth/api-auth.interceptor.ts
+++ src/app/core/interceptors/api-auth/api-auth.interceptor.ts
@@ -1,6 +1,6 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { API_AUTH_CONFIG } from '@tokens';
+import { API_AUTH_CONFIG } from '@tokens/api';
 
 /**
  * Interceptor function for API authentication.
@@ -12,20 +12,28 @@ import { API_AUTH_CONFIG } from '@tokens';
  * @returns The modified HTTP request.
  */
 export const apiAuthInterceptor: HttpInterceptorFn = (req, next) => {
-  const apiAuthConfig = inject(API_AUTH_CONFIG);
+  const apiAuthConfig = inject(API_AUTH_CONFIG).getValue();
+  const matchesUrl = req.url.includes(apiAuthConfig.match);
 
-  if (apiAuthConfig) {
-    const { addTo, key, authorizationType, value } = apiAuthConfig.getValue();
+  if (!matchesUrl) {
+    return next(req);
+  }
 
-    if (addTo === 'headers') {
-      req = req.clone({
-        headers: req.headers.set(key, `${authorizationType} ${value}`.trim())
-      });
-    } else if (addTo === 'params') {
-      req = req.clone({
-        params: req.params.set(key, value.trim())
-      });
-    }
+  if (!apiAuthConfig) {
+    throw new HttpErrorResponse({
+      status: 401,
+      statusText: 'API_AUTH_CONFIG is not set'
+    });
+  }
+
+  if (apiAuthConfig.addTo === 'headers') {
+    req = req.clone({
+      headers: req.headers.set(apiAuthConfig.key, `${apiAuthConfig.authorizationType} ${apiAuthConfig.value}`.trim())
+    });
+  } else if (apiAuthConfig.addTo === 'params') {
+    req = req.clone({
+      params: req.params.set(apiAuthConfig.key, apiAuthConfig.value.trim())
+    });
   }
 
   return next(req);
