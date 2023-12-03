989ef61b82e347293ab1437ee16a6fffc4cb0f08
mancarius <mattia.mancarella.dev@gmail.com>
2023-12-03 19:18:47 +0000

    Add media service authentication context token

--- src/app/tokens/api/index.ts
+++ src/app/tokens/api/index.ts
@@ -1,5 +1,16 @@
+import { HttpContextToken } from "@angular/common/http";
 import { InjectionToken } from "@angular/core";
 import { MediaServiceConfig } from "@shared/types";
 import { BehaviorSubject } from "rxjs";
 
+/**
+ * Injection token for the media service configuration.
+ * It is used to provide a BehaviorSubject of type MediaServiceConfig.
+ */
 export const MEDIA_SERVICE_CONFIG = new InjectionToken<BehaviorSubject<MediaServiceConfig>>('MEDIA_SERVICE_CONFIG');
+
+/**
+ * Token for the media service authentication.
+ * This context token is used to provide the configuration for the media service authentication to interceptors.
+ */
+export const MEDIA_SERVICE_AUTH = new HttpContextToken<MediaServiceConfig['authConfigs']>(() => []);
