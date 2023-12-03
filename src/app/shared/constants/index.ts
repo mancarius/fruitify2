6ad3540d82014f35a277a07ddc3b235fb8463271
mancarius <mattia.mancarella.dev@gmail.com>
2023-12-03 19:21:35 +0000

    Update media service config names

--- src/app/shared/constants/index.ts
+++ src/app/shared/constants/index.ts
@@ -5,7 +5,7 @@ export const API_BASE_PATHNAME = '/api/fruit';
 
 export const FRUITYVICE_PROXY_BASE_URL = environment.fruityviceProxy;
 
-export const PEXELS_API_AUTH_CONFIG: MediaServiceConfig = {
+export const PEXELS_API_CONFIG: MediaServiceConfig = {
   provider: MediaProvidersEnum.PEXELS,
   baseUrl: environment.pexelsApi,
   authConfigs: [
@@ -18,7 +18,7 @@ export const PEXELS_API_AUTH_CONFIG: MediaServiceConfig = {
   ]
 };
 
-export const UNSPLASH_API_AUTH_CONFIG: MediaServiceConfig = {
+export const UNSPLASH_API_CONFIG: MediaServiceConfig = {
   provider: MediaProvidersEnum.UNSPLASH,
   baseUrl: environment.unsplashApi,
   authConfigs: [
