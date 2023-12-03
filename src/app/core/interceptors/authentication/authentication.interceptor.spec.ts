55a8710553f509430d4e5d29afefc4b09f0ddec9
mancarius <mattia.mancarella.dev@gmail.com>
2023-12-03 20:08:34 +0000

    Add authentication interceptor unit tests

---
+++ src/app/core/interceptors/authentication/authentication.interceptor.spec.ts
@@ -0,0 +1,65 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpContext, HttpInterceptorFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
+import { authenticationInterceptor } from './authentication.interceptor';
+import { AUTH_CONFIG } from '@tokens/api';
+
+fdescribe('authenticationInterceptor', () => {
+  let httpTesting: HttpTestingController;
+  let httpClient: HttpClient;
+  const interceptor: HttpInterceptorFn = (req, next) =>
+    TestBed.runInInjectionContext(() => authenticationInterceptor(req, next));
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(withInterceptors([authenticationInterceptor])),
+        provideHttpClientTesting(),
+      ]
+    });
+
+    httpTesting = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
+  });
+
+  afterEach(() => {
+    httpTesting.verify();
+  });
+
+  it('should be created', () => {
+    expect(interceptor).toBeTruthy();
+  });
+
+  it('should add the authentication information to the request params', () => {
+    const req = new HttpRequest('GET', 'http://example', {
+      context: new HttpContext().set(AUTH_CONFIG, [{
+        addTo: 'params',
+        key: 'api_key',
+        value: '12345'
+      }])
+    });
+
+    httpClient.get(req.url, { context: req.context }).subscribe();
+
+    httpTesting.expectOne((req: HttpRequest<any>) => req.params.get('api_key') === '12345');
+
+    expect().nothing();
+  });
+
+  it('should add the authentication information to the request headers', () => {
+    const req = new HttpRequest('GET', 'http://example', {
+      context: new HttpContext().set(AUTH_CONFIG, [{
+        addTo: 'headers',
+        key: 'Authorization',
+        authorizationType: 'Bearer',
+        value: '12345'
+      }])
+    });
+
+    httpClient.get(req.url, { context: req.context }).subscribe();
+
+    httpTesting.expectOne((req: HttpRequest<any>) => req.headers.get('Authorization') === 'Bearer 12345');
+
+    expect().nothing();
+  });
+});
