43e6d24269a5a205a823478f0aacf3f6e1334e81
mancarius <mattia.mancarella.dev@gmail.com>
2023-12-03 19:18:20 +0000

    Refactor media service configuration

--- src/app/shared/services/media/media.service.spec.ts
+++ src/app/shared/services/media/media.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-
 import { MediaService } from './media.service';
+import { MEDIA_SERVICE_CONFIG } from '@tokens/api';
+import { PEXELS_API_CONFIG } from '@shared/constants';
+
 
 describe('MediaService', () => {
   let service: MediaService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: MEDIA_SERVICE_CONFIG,
+          useValue: PEXELS_API_CONFIG
+        }
+      ]
+    });
     service = TestBed.inject(MediaService);
   });
 
