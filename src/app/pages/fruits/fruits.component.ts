96826b65878e24ba4f1ad285654863eaecb20865
mancarius <mattia.mancarella.dev@gmail.com>
2023-12-05 17:34:26 +0000

    Add search functionality

--- src/app/pages/fruits/fruits.component.ts
+++ src/app/pages/fruits/fruits.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EffectRef, Injector, OnInit, Signal, effect, inject } from '@angular/core';
 import { FruitsSearchComponent } from './components/fruits-search/fruits-search.component';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Fruit } from '../../shared/types';
@@ -6,6 +6,7 @@ import { FruitsStore } from './fruits.store';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { LoadingService } from '../../shared/services/loading/loading.service';
 import { provideComponentStore } from '@ngrx/component-store';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-fruits',
@@ -21,18 +22,31 @@ import { provideComponentStore } from '@ngrx/component-store';
   templateUrl: './fruits.component.html',
 })
 export class FruitsComponent implements OnInit {
-  protected searchControl = new FormControl();
-  private _searchText$ = this.searchControl.valueChanges;
+  private readonly _fruitStore = inject(FruitsStore);
+  private readonly _loadingService = inject(LoadingService);
+  private readonly _router = inject(Router);
+  private readonly _injector = inject(Injector);
+
+  protected searchControl = new FormControl("");
+  private _searchText = toSignal(this.searchControl.valueChanges, { initialValue: "" });
 
   public fruits = toSignal<Fruit[]>(this._fruitStore.fruits$);
   public loading = toSignal<boolean>(this._loadingService.isLoading$);
 
-  constructor(
-    private readonly _fruitStore: FruitsStore,
-    private readonly _loadingService: LoadingService) { }
-
   ngOnInit() {
     this._fruitStore.fetchFruits();
-    this._fruitStore.getFruitByName(this._searchText$);
+
+    this._search(this._searchText);
+  }
+
+  /**
+   * Performs a search based on the provided text and navigates to the fruits page with the search query as a query parameter.
+   * @param text - The search text.
+   * @returns An EffectRef object representing the search effect.
+   */
+  private _search(text: Signal<string|null>): EffectRef {
+    return effect(() => {
+      this._router.navigate(['/fruits'], { queryParams: { name: text() } });
+    }, { injector: this._injector });
   }
 }
