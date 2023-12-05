863da55ffad9d10429e17cfc8e3ca0e88ea75de0
mancarius <mattia.mancarella.dev@gmail.com>
2023-12-05 16:57:46 +0000

    Add fruit service and resolver to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+import { FruitService } from '@shared/services/fruit/fruit.service';
+import { fruitResolver } from '@resolvers/fruit/fruit.resolver';
 
 export const routes: Routes = [
   {
@@ -8,9 +10,27 @@ export const routes: Routes = [
   },
   {
     path: 'fruits',
-    async loadComponent() {
-      const c = await import('./pages/fruits/fruits.component');
-      return c.FruitsComponent;
+    providers: [FruitService],
+    resolve: {
+      fruits: fruitResolver
     },
+    children: [
+      {
+        title: 'Fruits',
+        path: '',
+        loadChildren: async () => {
+          const c = await import('./pages/fruits/fruits.component');
+          return c.FruitsComponent;
+        },
+      },
+      {
+        title: 'Fruit',
+        path: '/:id/*',
+        loadChildren: async () => {
+          const c = await import('./pages/fruit/fruit.component');
+          return c.FruitComponent;
+        },
+      }
+    ]
   }
 ];
