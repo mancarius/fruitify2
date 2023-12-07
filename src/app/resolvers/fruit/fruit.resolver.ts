5a6e973dfc598a77bdfca3ac59fe4a2413fd4052
mancarius <mattia.mancarella.dev@gmail.com>
2023-12-07 18:26:03 +0000

    Add fruit resolver

--- src/app/resolvers/fruit/fruit.resolver.ts
+++ src/app/resolvers/fruit/fruit.resolver.ts
@@ -1,28 +1,23 @@
 import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
 import { FruitService } from '@shared/services/fruit/fruit.service';
-import { defer, finalize, map } from 'rxjs';
-import { Fruit, QueryParams } from '@shared/types';
+import { defer, finalize, of } from 'rxjs';
+import { Fruit } from '@shared/types';
 import { LoadingService } from '@shared/services/loading/loading.service';
 
-export const fruitResolver: ResolveFn<Fruit[]> = (route, state) => {
+export const fruitResolver: ResolveFn<Fruit|null> = (route, state) => {
   const service = inject(FruitService);
   const loader = inject(LoadingService);
   const fruitId = route.paramMap.get('id');
-  const query = route.queryParamMap as QueryParams<keyof Omit<Fruit, 'nutritions' | 'id'>>;
 
   loader.start();
 
   return defer(() => {
     if (fruitId) {
-      return service.getById(Number(fruitId)).pipe(map((fruit) => [fruit]));
+      return service.getById(Number(fruitId));
     }
 
-    if (Object.keys(query).length) {
-      return service.getWithQuery(query);
-    }
-
-    return service.getAll();
+    return of(null);
   }).pipe(
     finalize(() => loader.stop())
   );
