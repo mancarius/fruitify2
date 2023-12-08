eaa0793bef308644ff909dcdbf9601d19399842d
mancarius <mattia.mancarella.dev@gmail.com>
2023-12-08 11:49:30 +0000

    Refactor fetchPhoto method in FruitStore

--- src/app/pages/fruit/fruit.store.ts
+++ src/app/pages/fruit/fruit.store.ts
@@ -9,24 +9,51 @@ export class FruitStore extends ComponentStore<object> {
   constructor(private _mediaService: MediaService) {
     super();
 
-    effect(() => this.fetchPhoto(this.fruit()));
+    // I'm using `allowSignalWrites` here because I want to be able to set the photo
+    // from the `fetchPhoto` method. This isn't safe in general, but it's fine here.
+    effect(() => this.fetchPhoto(this.fruit()), {
+      allowSignalWrites: true
+    });
   }
+
+  /* ===== State ===== */
 
   readonly fruit = signal<Nullable<Fruit>>(null);
   readonly photo = signal<Nullable<MediaPhoto>>(null);
   readonly loading = signal<boolean>(false);
 
+
+  /* ===== Updaters ===== */
+
   readonly setFruit = (fruit?: Nullable<Fruit>) => this.fruit.set(fruit ?? null);
+
+
+  /* ===== Effects ===== */
 
   readonly fetchPhoto = this.effect((fruit$: Observable<Nullable<Fruit>>) => {
     return fruit$.pipe(
       tap(() => this.loading.set(true)),
-      switchMap((fruit) => fruit ? this._mediaService.findPhoto(fruit.name) : of(null)),
-      tapResponse(
-        this.photo.set,
-        (error) => console.error(error),
-        () => this.loading.set(false),
+      switchMap((fruit) => this._findPhoto(fruit)
+        .pipe(
+          tapResponse(
+            this.photo.set,
+            (error) => console.error(error),
+            () => this.loading.set(false),
+          ),
+        )
       ),
     );
   });
+
+
+  /* ===== Private Methods ===== */
+
+  /**
+   * Finds the photo for a given fruit.
+   * @param fruit - The fruit to find the photo for.
+   * @returns An observable that emits the photo if found, or null if not found.
+   */
+  private _findPhoto(fruit: Nullable<Fruit>): Observable<Nullable<MediaPhoto>> {
+    return fruit ? this._mediaService.findPhoto(fruit.name) : of(null);
+  }
 }
