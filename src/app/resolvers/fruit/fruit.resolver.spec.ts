1f1875ae7e291104f7dac099ec4b0230de48f069
mancarius <mattia.mancarella.dev@gmail.com>
2023-12-08 11:50:27 +0000

    Update fruitResolver to use Nullable<Fruit> type

--- src/app/resolvers/fruit/fruit.resolver.spec.ts
+++ src/app/resolvers/fruit/fruit.resolver.spec.ts
@@ -1,10 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { ResolveFn } from '@angular/router';
-
 import { fruitResolver } from './fruit.resolver';
+import { Fruit, Nullable } from '@shared/types';
 
 describe('fruitResolver', () => {
-  const executeResolver: ResolveFn<boolean> = (...resolverParameters) => 
+  const executeResolver: ResolveFn<Nullable<Fruit>> = (...resolverParameters) => 
       TestBed.runInInjectionContext(() => fruitResolver(...resolverParameters));
 
   beforeEach(() => {
