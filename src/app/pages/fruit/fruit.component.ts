1b7111d94146fa53aa3eace34b16b44eed8af502
mancarius <mattia.mancarella.dev@gmail.com>
2023-12-08 12:14:15 +0000

    Refactor fruit component imports and update photo store usage

--- src/app/pages/fruit/fruit.component.ts
+++ src/app/pages/fruit/fruit.component.ts
@@ -1,29 +1,37 @@
-import { Component, effect, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Data } from '@angular/router';
-import { FruitStore } from './fruit.store';
+import { PhotoStore } from './store/photo.store';
 import { provideComponentStore } from '@ngrx/component-store';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Fruit, Nullable } from '@shared/types';
+import { Observable, filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-fruit',
   standalone: true,
   imports: [CommonModule],
-  providers: [provideComponentStore(FruitStore)],
+  providers: [provideComponentStore(PhotoStore)],
   templateUrl: './fruit.component.html',
   styleUrl: './fruit.component.scss'
 })
 export class FruitComponent {
+
   private _activatedRoute = inject(ActivatedRoute);
-  private _cs = inject(FruitStore);
-  private _routeData = toSignal<Data | { fruit: Nullable<Fruit> }>(this._activatedRoute.data);
+  private _photoStore = inject(PhotoStore);
+  private _routeData$: Observable<Data | { fruit: Nullable<Fruit> }> = this._activatedRoute.data;
+  private _fruit$: Observable<Nullable<Fruit>> = this._routeData$.pipe(
+    filter((data) => 'fruit' in data),
+    map((data) => data.fruit)
+  );
 
-  protected fruit = this._cs.fruit;
-  protected photo = this._cs.photo;
-  protected loading = this._cs.loading;
+  /* Public properties */
+  fruit = toSignal(this._fruit$, { initialValue: null });
+  photo = this._photoStore.photo;
+  loading = this._photoStore.loading;
 
-  constructor() {
-    effect(() => this._cs.setFruit(this._routeData()?.fruit));
+
+  ngOnInit() {
+    this._photoStore.fetchPhoto(this._fruit$);
   }
 }
