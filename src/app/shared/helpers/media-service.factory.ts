615c04a85cc1cdd6095562865d19f3ca77ee8e8b
mancarius <mattia.mancarella.dev@gmail.com>
2023-12-11 12:08:39 +0000

    Fix mediaServiceFactory to use BehaviorSubject for mediaServiceConfig

--- src/app/shared/helpers/media-service.factory.ts
+++ src/app/shared/helpers/media-service.factory.ts
@@ -3,7 +3,8 @@ import { PexelsService } from '../services/pexels/pexels.service';
 import { HttpClient } from '@angular/common/http';
 import { MediaService } from '../services/media/media.service';
 import { UnsplashService } from '@shared/services/unsplash/unsplash.service';
-import { AbstractMediaProviderService } from '@shared/services/abstract-provider/abstract-provider.service';
+import { AbstractMediaProviderService } from '@shared/services/abstract-media-provider/abstract-media-provider.service';
+import { BehaviorSubject } from 'rxjs';
 
 
 /**
@@ -12,18 +13,19 @@ import { AbstractMediaProviderService } from '@shared/services/abstract-provider
  * @param http - The HttpClient instance.
  * @returns A media service instance.
  */
-export function mediaServiceFactory(mediaServiceConfig: MediaServiceConfig, http: HttpClient): MediaService {
+export function mediaServiceFactory(mediaServiceConfig: BehaviorSubject<MediaServiceConfig>, http: HttpClient): MediaService {
+  const provider = mediaServiceConfig.value.provider;
   let mediaProvider: AbstractMediaProviderService;
   
-  switch (mediaServiceConfig.provider) {
+  switch (provider) {
     case MediaProvidersEnum.PEXELS:
-      mediaProvider = new PexelsService(mediaServiceConfig, http);
+      mediaProvider = new PexelsService(mediaServiceConfig.value, http);
       break;
     case MediaProvidersEnum.UNSPLASH:
-      mediaProvider = new UnsplashService(mediaServiceConfig, http);
+      mediaProvider = new UnsplashService(mediaServiceConfig.value, http);
       break;
     default:
-      throw new Error(`Media provider '${mediaServiceConfig.provider}' not supported.`);
+      throw new Error(`Media provider '${provider}' not supported.`);
   }
 
   return new MediaService(mediaProvider);
