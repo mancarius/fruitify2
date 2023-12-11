e1d257cf6a46daeb1b824e230fa0df283430d20b
mancarius <mattia.mancarella.dev@gmail.com>
2023-12-11 14:56:38 +0000

    Add router links to fruit list component and fix styles

--- src/app/shared/components/fruit-list/fruit-list.component.ts
+++ src/app/shared/components/fruit-list/fruit-list.component.ts
@@ -3,34 +3,44 @@ import { CommonModule } from '@angular/common';
 import { Fruit } from '@shared/types';
 import { FruitPreviewComponent } from '../fruit-preview/fruit-preview.component';
 import { FruitPreviewPlaceholderComponent } from '@shared/components/fruit-preview-placeholder/fruit-preview-placeholder.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-fruit-list',
   standalone: true,
-  imports: [CommonModule, FruitPreviewComponent, FruitPreviewPlaceholderComponent],
+  imports: [CommonModule, FruitPreviewComponent, FruitPreviewPlaceholderComponent, RouterLink, RouterLinkActive],
   changeDetection: ChangeDetectionStrategy.OnPush,
 
   template: `
-    @for (fruit of fruits(); track fruit.id) {
-      @defer (on viewport) {
-        <app-fruit-preview class="aspect-[3/2] flex justify-center items-center" [fruit]="fruit"></app-fruit-preview>
-      } @placeholder {
-        <app-fruit-preview-placeholder></app-fruit-preview-placeholder>
+    <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      @for (fruit of fruits(); track fruit.id) {
+        <li class="flex flex-col">
+          @defer (on viewport) {
+            <a class="aspect-[3/2] flex"
+              [routerLink]="['/fruits', fruit.name.toLocaleLowerCase()]"
+              [queryParams]="{ fruitId: fruit.id }"
+              routerLinkActive="active"
+            >
+              <app-fruit-preview class="grow flex justify-center items-center" [fruit]="fruit"></app-fruit-preview>
+            </a>
+          } @placeholder {
+            <app-fruit-preview-placeholder></app-fruit-preview-placeholder>
+          }
+        </li>
+      } @empty {
+        <li class="col-span-full">
+          <p class="text-center bg-white/80 w-fit px-2 mx-auto">No fruits found</p>
+        </li>
       }
-    }
+    </ul>
   `,
 
-  styles: [
-    `:host { 
-      display: grid; 
-      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); 
-      grid-gap: 1rem; 
-    }`
-  ]
+  styles: [`:host { display: block; }`]
+
 })
 export class FruitListComponent {
   @Input({ alias: `fruits`, required: true })
   set _fruits(value: Fruit[]) { this.fruits.set(value) }
 
-  protected fruits = signal<Fruit[]>([]);
+  fruits = signal<Fruit[]>([]);
 }
