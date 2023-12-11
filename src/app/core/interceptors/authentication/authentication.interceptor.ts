347939276a2db286b05659e5a55b7a49f88f85e2
mancarius <mattia.mancarella.dev@gmail.com>
2023-12-11 11:56:12 +0000

    Update authentication interceptor imports

--- src/app/core/interceptors/authentication/authentication.interceptor.ts
+++ src/app/core/interceptors/authentication/authentication.interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { setRequestAuth } from './authentication.utils';
-import { AUTH_CONFIG } from '@tokens/api';
+import { AUTH_CONFIG_CONTEXT_TOKEN } from '@tokens';
 
 /**
  * Interceptor function for handling authentication in HTTP requests based on the provided configuration.
@@ -9,6 +9,6 @@ import { AUTH_CONFIG } from '@tokens/api';
  * @returns The modified HTTP request with authentication information.
  */
 export const authenticationInterceptor: HttpInterceptorFn = (req, next) => {
-  const config = req.context.get(AUTH_CONFIG);
+  const config = req.context.get(AUTH_CONFIG_CONTEXT_TOKEN);
   return next(config ? setRequestAuth(req, config) : req);
 };
