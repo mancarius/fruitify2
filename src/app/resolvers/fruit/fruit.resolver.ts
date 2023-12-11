024ea4ae0760467a15d75cbd5c679de403e32b60
mancarius <mattia.mancarella.dev@gmail.com>
2023-12-11 14:55:53 +0000

    Fix fruitResolver to handle errors

--- src/app/resolvers/fruit/fruit.resolver.ts
+++ src/app/resolvers/fruit/fruit.resolver.ts
@@ -1,24 +1,22 @@
 import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
 import { FruitService } from '@shared/services/fruit/fruit.service';
-import { defer, finalize, of } from 'rxjs';
+import { catchError, defer, finalize, of } from 'rxjs';
 import { Fruit } from '@shared/types';
 import { LoadingService } from '@shared/services/loading/loading.service';
 
-export const fruitResolver: ResolveFn<Fruit|null> = (route, state) => {
+export const fruitResolver: ResolveFn<Fruit | null> = (route, state) => {
   const service = inject(FruitService);
   const loader = inject(LoadingService);
-  const fruitId = route.paramMap.get('id');
+  const fruitId = route.queryParamMap.get('fruitId');
 
   loader.start();
 
-  return defer(() => {
-    if (fruitId) {
-      return service.getById(Number(fruitId));
-    }
-
-    return of(null);
-  }).pipe(
-    finalize(() => loader.stop())
+  return defer(() => fruitId
+    ? service.getById(Number(fruitId))
+    : of(null)
+  ).pipe(
+    finalize(() => loader.stop()),
+    catchError(() => of(null))
   );
 };
