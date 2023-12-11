4fab6b9cd49821440ae65bfb4d92587332a8cf86
mancarius <mattia.mancarella.dev@gmail.com>
2023-12-11 11:51:32 +0000

    Rename tokens

--- src/app/tokens/index.ts
+++ src/app/tokens/index.ts
@@ -7,10 +7,10 @@ import { BehaviorSubject } from "rxjs";
  * Injection token for the media service configuration.
  * It is used to provide a BehaviorSubject of type MediaServiceConfig.
  */
-export const MEDIA_SERVICE_CONFIG = new InjectionToken<BehaviorSubject<MediaServiceConfig>>('MEDIA_SERVICE_CONFIG');
+export const MEDIA_SERVICE_CONFIG_TOKEN = new InjectionToken<BehaviorSubject<MediaServiceConfig|null>>('MEDIA_SERVICE_CONFIG');
 
 /**
  * Context token for the authentication configuration.
  * It is used to provide a function that returns an array of authentication configurations.
  */
-export const AUTH_CONFIG = new HttpContextToken<MediaServiceConfig['authConfigs']>(() => []);
+export const AUTH_CONFIG_CONTEXT_TOKEN = new HttpContextToken<MediaServiceConfig['authConfigs']>(() => []);
