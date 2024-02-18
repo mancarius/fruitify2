114156aa8d79aa81a5f4fe30a1b40caa0d78c687
mancarius <mattia.mancarella.dev@gmail.com>
2024-02-18 21:59:58 +0000

    Add GitHub icon to navbar component

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { RouterLink } from '@angular/router';
 import { ThemeTogglerComponent } from '@shared/components/theme-toggler/theme-toggler.component';
 
@@ -33,13 +34,22 @@ import { ThemeTogglerComponent } from '@shared/components/theme-toggler/theme-to
           class="lg:flex items-center bg-white dark:bg-gray-900 lg:bg-transparent lg:dark:bg-transparent lg:shadow-none"
           [ngClass]="{'hidden': !showMenu, 'block': showMenu}">
           <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
+
             <li class="flex items-center">
-              <a class="lg:text-white lg:hover:text-blue-950 dark:text-white dark:hover:text-blue-950 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold w-full "
+              <a class="lg:text-white lg:hover:text-blue-950 dark:text-white dark:hover:text-blue-950 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold w-full"
                 routerLink="/settings">
                 <mat-icon class="text-blue-950 dark:text-white opacity-80">settings</mat-icon>
                 <span class="lg:hidden inline-block ml-2">Settings</span>
               </a>
             </li>
+
+            <li class="flex items-center">
+              <a [href]="ghLink" class="lg:text-white lg:hover:text-blue-950 dark:text-white dark:hover:text-blue-950 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold w-full">
+                <mat-icon svgIcon="github" class="text-blue-950 dark:text-white opacity-80"></mat-icon>
+                <span class="lg:hidden inline-block ml-2">Github</span>
+              </a>
+            </li>
+
           </ul>
         </div>
       </div>
@@ -47,7 +57,15 @@ import { ThemeTogglerComponent } from '@shared/components/theme-toggler/theme-to
   `,
 })
 export class NavbarComponent {
+  #matIconRegistry = inject(MatIconRegistry);
+  #domSanitizer = inject(DomSanitizer);
+
   protected showMenu = false;
+  protected ghLink = 'https://github.com/mancarius/fruitify2';
+
+  constructor() {
+    this.#matIconRegistry.addSvgIcon('github', this.#domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/github.svg'));
+  }
 
   toggleNavbar(){
     this.showMenu = !this.showMenu;
