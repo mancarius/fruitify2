a534a85e81ae13d28d328ebf82997dae713bb413
mancarius <mattia.mancarella.dev@gmail.com>
2024-03-16 22:11:39 +0000

    Refactor RelatedFruitsComponent to use private property instead of readonly property

--- src/app/shared/components/related-fruits/related-fruits.component.ts
+++ src/app/shared/components/related-fruits/related-fruits.component.ts
@@ -54,24 +54,25 @@ export class RelatedFruitsContentDirective {
   `
 })
 export class RelatedFruitsComponent {
-  readonly #cs = inject(RelatedFruitsStore);
+  private readonly _cs = inject(RelatedFruitsStore);
 
-  @ContentChild(RelatedFruitsContentDirective) content!: RelatedFruitsContentDirective;
+  @ContentChild(RelatedFruitsContentDirective)
+  readonly content!: RelatedFruitsContentDirective;
 
   @Input({ required: true })
-  set fruit(value: Nullable<Fruit>) { this.#cs.setFruit(value); }
+  set fruit(value: Nullable<Fruit>) { this._cs.setFruit(value); }
 
   @Input()
-  set maxSuggestions(value: number | undefined) { typeof value === "number" && this.#cs.setMaxSuggestions(value); }
+  set maxSuggestions(value: number | undefined) { typeof value === "number" && this._cs.setMaxSuggestions(value); }
 
-  readonly loading = toSignal(this.#cs.select(state => state.loading));
+  readonly loading = toSignal(this._cs.select(state => state.loading));
 
-  readonly fruits = toSignal(this.#cs.fruits$);
+  readonly fruits = toSignal(this._cs.fruits$);
 
   /**
    * Determines whether the "Load More" button should be shown based on the maximum suggestions and the number of fruits.
    */
-  readonly showLoadMoreBtn = toSignal(this.#cs.select(state => state.maxSuggestions < state.fruits.length));
+  readonly showLoadMoreBtn = toSignal(this._cs.select(state => state.maxSuggestions < state.fruits.length));
 
-  readonly showAll = this.#cs.showAll;
+  readonly showAll = this._cs.showAll;
 }
