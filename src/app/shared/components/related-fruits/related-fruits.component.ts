51e15f3ae318dd01792b67aa91d512c0e10a5583
mancarius <mattia.mancarella.dev@gmail.com>
2024-10-03 00:14:23 +0200

    Refactor RelatedFruitsComponent template

--- src/app/shared/components/related-fruits/related-fruits.component.ts
+++ src/app/shared/components/related-fruits/related-fruits.component.ts
@@ -1,11 +1,18 @@
-import { ChangeDetectionStrategy, Component, ContentChild, Directive, input, TemplateRef, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RelatedFruitsStore } from './related-fruits.store';
-import { Fruit, Nullable } from '@shared/types';
-import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
-import { MAX_SUGGESTIONS_PREVIEW_OPTION } from '@tokens';
-
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ContentChild,
+  Directive,
+  input,
+  TemplateRef,
+  inject,
+} from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RelatedFruitsStore } from "./related-fruits.store";
+import { Fruit, Nullable } from "@shared/types";
+import { MatButtonModule } from "@angular/material/button";
+import { RouterLink } from "@angular/router";
+import { MAX_SUGGESTIONS_PREVIEW_OPTION } from "@tokens";
 
 /**
  * Directive to be used as a template for the related fruits component
@@ -20,42 +27,55 @@ import { MAX_SUGGESTIONS_PREVIEW_OPTION } from '@tokens';
  * ```
  */
 @Directive({
-  selector: '[appRelatedFruitsContent]',
-  standalone: true
+  selector: "[appRelatedFruitsContent]",
+  standalone: true,
 })
 export class RelatedFruitsContentDirective {
   readonly templateRef = inject(TemplateRef);
 }
 
-
-
 @Component({
-  selector: 'app-related-fruits',
+  selector: "app-related-fruits",
   standalone: true,
   imports: [CommonModule, MatButtonModule, RouterLink],
   providers: [RelatedFruitsStore],
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    class: 'flex flex-col gap-4'
+    class: "flex flex-col gap-4",
   },
   template: `
     @if (cs.loading()) {
-      <p class="text-slate-400 dark:text-slate-500 center">Loading...</p>
+      <p
+        class="text-slate-400 dark:text-slate-500 center"
+        data-test-id="loading"
+      >
+        Loading...
+      </p>
     } @else if (cs.error()) {
-      <p class="text-red-400 dark:text-red-500 center">{{ cs.error() }}</p>
+      <p
+        class="text-red-400 dark:text-red-500 center"
+        data-test-id="error-message"
+      >
+        {{ cs.error() }}
+      </p>
     } @else {
-      <ng-container [ngTemplateOutlet]="content.templateRef" [ngTemplateOutletContext]="{ $implicit: cs.slicedFruits() }"></ng-container>
+      <ng-container
+        [ngTemplateOutlet]="content.templateRef"
+        [ngTemplateOutletContext]="{ $implicit: cs.slicedFruits() }"
+      ></ng-container>
 
-      @if (cs.showLoadMoreBtn()){
-        <div class="flex justify-center">
-          <a mat-button
+      @if (cs.showLoadMoreBtn()) {
+        <div class="flex justify-center" data-test-id="btn-container">
+          <a
+            mat-button
             class="text-slate-900 dark:text-slate-200"
             (click)="cs.toggleShowAll()"
-          >{{ cs.showAll() ? "Show less" : "Show more" }}</a>
+            >{{ cs.showAll() ? "Show less" : "Show more" }}</a
+          >
         </div>
       }
     }
-  `
+  `,
 })
 export class RelatedFruitsComponent {
   protected readonly cs = inject(RelatedFruitsStore);
@@ -65,7 +85,9 @@ export class RelatedFruitsComponent {
 
   readonly fruit = input.required<Nullable<Fruit>>();
 
-  readonly maxSuggestions = input<number>(inject(MAX_SUGGESTIONS_PREVIEW_OPTION));
+  readonly maxSuggestions = input<number>(
+    inject(MAX_SUGGESTIONS_PREVIEW_OPTION),
+  );
 
   constructor() {
     this.cs.fetchFruits(this.fruit);
