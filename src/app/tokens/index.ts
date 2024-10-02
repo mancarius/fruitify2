8d6b4cb3d48f109d7eb6aef45eb528fe02442d78
mancarius <mattia.mancarella.dev@gmail.com>
2024-10-03 00:13:32 +0200

    refactor(tokens): Add PHOTO_DEFAULT_OPTIONS injection token

--- src/app/tokens/index.ts
+++ src/app/tokens/index.ts
@@ -1,20 +1,45 @@
 import { HttpContextToken } from "@angular/common/http";
 import { InjectionToken, WritableSignal } from "@angular/core";
-import { MediaServiceConfig, Nullable } from "@shared/types";
+import {
+  MediaServiceConfig,
+  Nullable,
+  MediaOptions,
+  MediaOrientation,
+} from "@shared/types";
 
 /**
  * Injection token for the media service configuration.
  * It is used to provide a BehaviorSubject of type MediaServiceConfig.
  */
-export const MEDIA_SERVICE_CONFIG_TOKEN = new InjectionToken<WritableSignal<Nullable<MediaServiceConfig>>>('MEDIA_SERVICE_CONFIG');
+export const MEDIA_SERVICE_CONFIG_TOKEN = new InjectionToken<
+  WritableSignal<Nullable<MediaServiceConfig>>
+>("MEDIA_SERVICE_CONFIG");
 
 /**
  * Context token for the authentication configuration.
  * It is used to provide a function that returns an array of authentication configurations.
  */
-export const AUTH_CONFIG_CONTEXT_TOKEN = new HttpContextToken<MediaServiceConfig['authConfigs']>(() => []);
+export const AUTH_CONFIG_CONTEXT_TOKEN = new HttpContextToken<
+  MediaServiceConfig["authConfigs"]
+>(() => []);
 
 /**
  * Injection token for the maximum number of suggestions to show in the preview.
  */
-export const MAX_SUGGESTIONS_PREVIEW_OPTION = new InjectionToken<number>('MAX_SUGGESTIONS_PREVIEW_OPTION');
+export const MAX_SUGGESTIONS_PREVIEW_OPTION = new InjectionToken<number>(
+  "MAX_SUGGESTIONS_PREVIEW_OPTION",
+);
+
+/**
+ * Injection token for the default options for the photo search.
+ */
+export const PHOTO_DEFAULT_OPTIONS = new InjectionToken<Partial<MediaOptions>>(
+  "PHOTO_DEFAULT_OPTIONS",
+  {
+    providedIn: "root",
+    factory: () => ({
+      limit: 1,
+      orientation: MediaOrientation.LANDSCAPE,
+    }),
+  },
+);
