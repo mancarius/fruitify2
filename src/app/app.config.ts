31f5a9614b7e130a6f9ffd64d2a976cf5b440b68
mancarius <mattia.mancarella.dev@gmail.com>
2024-10-03 23:26:24 +0200

    Refactor app.config.ts to improve code structure and formatting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,49 +1,60 @@
-import { ApplicationConfig, isDevMode, provideExperimentalZonelessChangeDetection, signal } from '@angular/core';
-import { provideRouter, withComponentInputBinding, withRouterConfig, withViewTransitions } from '@angular/router';
-import routes from './app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
-import { provideServiceWorker } from '@angular/service-worker';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { MediaServiceConfig } from '@shared/types';
-import { MEDIA_SERVICE_CONFIG_TOKEN } from '@tokens';
-import { authenticationInterceptor } from '@core/interceptors';
-import { PEXELS_API_CONFIG } from '@shared/constants';
+import {
+  ApplicationConfig,
+  isDevMode,
+  provideExperimentalZonelessChangeDetection,
+  signal,
+  inject,
+} from "@angular/core";
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withRouterConfig,
+  withViewTransitions,
+} from "@angular/router";
+import routes from "./app.routes";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material/form-field";
+import { provideServiceWorker } from "@angular/service-worker";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { MediaServiceConfig } from "@shared/types";
+import { MEDIA_SERVICE_CONFIG_TOKEN } from "@tokens";
+import { authenticationInterceptor } from "@core/interceptors";
+import { PEXELS_API_CONFIG } from "@shared/constants";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-        provideExperimentalZonelessChangeDetection(),
+    provideExperimentalZonelessChangeDetection(),
 
-        provideRouter(routes,
-            withComponentInputBinding(),
-            withViewTransitions(),
-            withRouterConfig({ paramsInheritanceStrategy: 'always' })
-        ),
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withViewTransitions(),
+      withRouterConfig({ paramsInheritanceStrategy: "always" }),
+    ),
 
-        provideAnimations(),
+    provideAnimations(),
 
-        provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-        }),
+    provideServiceWorker("ngsw-worker.js", {
+      enabled: !isDevMode(),
+      registrationStrategy: "registerWhenStable:30000",
+    }),
 
-        provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-        }),
+    provideServiceWorker("ngsw-worker.js", {
+      enabled: !isDevMode(),
+      registrationStrategy: "registerWhenStable:30000",
+    }),
 
-        provideHttpClient(
-            withInterceptors([authenticationInterceptor])
-        ),
+    provideHttpClient(withInterceptors([authenticationInterceptor])),
 
-        {
-            provide: MEDIA_SERVICE_CONFIG_TOKEN,
-            useFactory: () => signal<MediaServiceConfig | null>(PEXELS_API_CONFIG)
-        },
+    {
+      provide: MEDIA_SERVICE_CONFIG_TOKEN,
+      useFactory: () =>
+        signal<MediaServiceConfig | null>(inject(PEXELS_API_CONFIG)),
+    },
 
-        {
-            provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
-            useValue: { appearance: 'outline' }
-        },
-    ]
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: "outline" },
+    },
+  ],
 };
