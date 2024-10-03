7a2564c45f9c93fbb90ede1f72b29059e9b1b724
mancarius <mattia.mancarella.dev@gmail.com>
2024-10-04 00:21:58 +0200

    Refactor FruitsStore to use signal store intead of component store

--- src/app/pages/fruits/fruits.store.ts
+++ src/app/pages/fruits/fruits.store.ts
@@ -1,62 +1,39 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRoute, Params, Router } from "@angular/router";
-import { ComponentStore } from "@ngrx/component-store";
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { patchState, signalStore, withMethods, withState } from "@ngrx/signals";
+import { rxMethod } from "@ngrx/signals/rxjs-interop";
 import { Fruit, Nullable, SearchContext } from "@shared/types";
-import { Observable, map, shareReplay, tap } from "rxjs";
+import { Observable, tap } from "rxjs";
 
-export type HomeState = {
-  fruits: Fruit[],
-  queryParams: Nullable<Params & Record<SearchContext, Nullable<string>>>,
+export type FruitsState = {
+  fruits: Fruit[];
 };
 
-export const homeInitialState: HomeState = {
+export const initialState: FruitsState = {
   fruits: [],
-  queryParams: null,
 };
 
-@Injectable()
-export class FruitsStore extends ComponentStore<HomeState>{
-  constructor(
-    private _router: Router,
-    private _activatedRoute: ActivatedRoute,
-  ) {
-    super(homeInitialState);
+export const fruitsStore = signalStore(
+  withState(initialState),
 
-    this.setQueryParams(this.routeQueryParams$);
-  }
+  withMethods((store, router = inject(Router)) => ({
+    setFruits: rxMethod<Fruit[]>((fruits$) =>
+      fruits$.pipe(
+        tap((fruits) => {
+          patchState(store, { fruits });
+        }),
+      ),
+    ),
 
-  /* ========== Selectors ========== */
-
-  readonly routeQueryParams$ = this._activatedRoute.queryParams.pipe(map((queryParams) => queryParams as Params & Record<SearchContext, Nullable<string>>));
-
-  readonly vm = this.selectSignal(state => ({
-    fruits: state.fruits
-  }));
-
-  /* ========== Updaters ========== */
-
-  readonly setFruits = this.updater((state, fruits: Fruit[]) => {
-    return { ...state, fruits };
-  });
-
-  readonly setQueryParams = this.updater((state, queryParams: Nullable<Params & Record<SearchContext, Nullable<string>>>) => {
-    return { ...state, queryParams };
-  });
-
-  /* ========== Effects ========== */
-
-  /**
-   * Performs a search based on the provided query parameters and navigates to the home page with the updated query parameters.
-   * @param query$ - An observable that emits the query parameters for the search.
-   * @returns An observable that emits the updated query parameters after navigating to the home page.
-   */
-  readonly search = this.effect((query$: Observable<Record<SearchContext, Nullable<string>> | null>) => {
-    return query$.pipe(
-      tap((query) => {
-        const queryParams = query ?? {};
-        this._router.navigate(["/"], { queryParams });
-      })
-    );
-  });
-
-}
+    search: rxMethod(
+      (query$: Observable<Record<SearchContext, Nullable<string>> | null>) => {
+        return query$.pipe(
+          tap((query) => {
+            const queryParams = query ?? {};
+            router.navigate(["/"], { queryParams });
+          }),
+        );
+      },
+    ),
+  })),
+);
