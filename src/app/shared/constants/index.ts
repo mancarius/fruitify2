72430c20c4dc7392058231f555cbaf8f40c5ae2c
mancarius <mattia.mancarella.dev@gmail.com>
2024-10-03 23:21:48 +0200

    Refactor app constants to Angular injection tokens

--- src/app/shared/constants/index.ts
+++ src/app/shared/constants/index.ts
@@ -1,81 +1,134 @@
+import { InjectionToken, inject } from "@angular/core";
 import { environment } from "@env/environment.development";
-import { MediaProvidersEnum, MediaServiceConfig, MediaServiceProviderCollection, Nutritions } from "@shared/types";
+import {
+  MediaProvidersEnum,
+  MediaServiceConfig,
+  MediaServiceProviderCollection,
+  Nutritions,
+} from "@shared/types";
 
 /**
  * The base pathname for the API endpoints related to fruit.
  */
-export const API_BASE_PATHNAME = '/api/fruit';
+export const API_BASE_PATHNAME = new InjectionToken<string>(
+  "API_BASE_PATHNAME_TOKEN",
+  {
+    providedIn: "root",
+    factory: () => "/api/fruit",
+  },
+);
 
 /**
  * The base URL for the Fruityvice proxy.
  */
-export const FRUITYVICE_PROXY_BASE_URL = environment.fruityviceProxy;
-
-export const MEDIA_SERVICE_PROVIDERS: MediaServiceProviderCollection = {
-  [MediaProvidersEnum.PEXELS]: {
-    name: MediaProvidersEnum.PEXELS,
-    link: 'https://www.pexels.com/',
-    description: 'The best free stock photos & videos shared by talented creators.'
+export const FRUITYVICE_PROXY_BASE_URL = new InjectionToken<string>(
+  "FRUITYVICE_PROXY_BASE_URL_TOKEN",
+  {
+    providedIn: "root",
+    factory: () => environment.fruityviceProxy,
   },
-  [MediaProvidersEnum.UNSPLASH]: {
-    name: MediaProvidersEnum.UNSPLASH,
-    link: 'https://www.unsplash.com/',
-    description: "The internet's source of freely-usable images."
-  }
-};
+);
+
+export const MEDIA_SERVICE_PROVIDERS =
+  new InjectionToken<MediaServiceProviderCollection>(
+    "MEDIA_SERVICE_PROVIDERS_TOKEN",
+    {
+      providedIn: "root",
+      factory: () => ({
+        [MediaProvidersEnum.PEXELS]: {
+          name: MediaProvidersEnum.PEXELS,
+          link: "https://www.pexels.com/",
+          description:
+            "The best free stock photos & videos shared by talented creators.",
+        },
+        [MediaProvidersEnum.UNSPLASH]: {
+          name: MediaProvidersEnum.UNSPLASH,
+          link: "https://www.unsplash.com/",
+          description: "The internet's source of freely-usable images.",
+        },
+      }),
+    },
+  );
 
 /**
  * Configuration object for Pexels API.
  */
-export const PEXELS_API_CONFIG: MediaServiceConfig = {
-  provider: MediaProvidersEnum.PEXELS,
-  baseUrl: environment.pexelsApi,
-  authConfigs: [
-    {
-      addTo: "headers",
-      key: "Authorization",
-      authorizationType: "",
-      value: environment.pexelsApiKey
-    }
-  ]
-};
+export const PEXELS_API_CONFIG = new InjectionToken<MediaServiceConfig>(
+  "PEXELS_API_CONFIG_TOKEN",
+  {
+    providedIn: "root",
+    factory: () => ({
+      provider: MediaProvidersEnum.PEXELS,
+      baseUrl: environment.pexelsApi,
+      authConfigs: [
+        {
+          addTo: "headers",
+          key: "Authorization",
+          authorizationType: "",
+          value: environment.pexelsApiKey,
+        },
+      ],
+    }),
+  },
+);
 
 /**
  * Configuration object for the Unsplash API.
  */
-export const UNSPLASH_API_CONFIG: MediaServiceConfig = {
-  provider: MediaProvidersEnum.UNSPLASH,
-  baseUrl: environment.unsplashApi,
-  authConfigs: [
-    {
-      addTo: "headers",
-      key: "Authorization",
-      authorizationType: "Client-ID",
-      value: environment.unsplashApiKey
-    }
-  ]
-};
+export const UNSPLASH_API_CONFIG = new InjectionToken<MediaServiceConfig>(
+  "UNSPLASH_API_CONFIG_TOKEN",
+  {
+    providedIn: "root",
+    factory: () => ({
+      provider: MediaProvidersEnum.UNSPLASH,
+      baseUrl: environment.unsplashApi,
+      authConfigs: [
+        {
+          addTo: "headers",
+          key: "Authorization",
+          authorizationType: "Client-ID",
+          value: environment.unsplashApiKey,
+        },
+      ],
+    }),
+  },
+);
 
 /**
  * Array of media service configurations.
  * @type {Array<MediaServiceConfig>}
  */
-export const API_CONFIGS: Array<MediaServiceConfig> = [PEXELS_API_CONFIG, UNSPLASH_API_CONFIG];
-
+export const API_CONFIGS = new InjectionToken<Array<MediaServiceConfig>>(
+  "API_CONFIGS_TOKEN",
+  {
+    providedIn: "root",
+    factory: () => [inject(PEXELS_API_CONFIG), inject(UNSPLASH_API_CONFIG)],
+  },
+);
 
 /**
  * Defines the colors associated with different nutrition types.
  */
-export const NUTRITION_COLORS: Record<keyof Nutritions, string> = {
-  carbohydrates: '#fbbf24',
-  protein: '#f87171',
-  fat: '#60a5fa',
-  calories: '#34d399',
-  sugar: '#f472b6',
-};
-
+export const NUTRITION_COLORS = new InjectionToken<
+  Record<keyof Nutritions, string>
+>("NUTRITION_COLORS_TOKEN", {
+  providedIn: "root",
+  factory: () => ({
+    carbohydrates: "#fbbf24",
+    protein: "#f87171",
+    fat: "#60a5fa",
+    calories: "#34d399",
+    sugar: "#f472b6",
+  }),
+});
 
 /**
  * The default color for the nutrition.
  */
-export const NUTRITION_DEFAULT_COLOR: string = '#e0e0e0';
+export const NUTRITION_DEFAULT_COLOR = new InjectionToken<string>(
+  "NUTRITION_DEFAULT_COLOR_TOKEN",
+  {
+    providedIn: "root",
+    factory: () => "#e0e0e0",
+  },
+);
