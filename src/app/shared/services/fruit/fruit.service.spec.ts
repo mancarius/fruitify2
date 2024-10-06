e519c12df01e1c39e91c5b41729982e271de4b05
mancarius <mattia.mancarella.dev@gmail.com>
2024-10-06 17:41:18 +0200

    Refactor FruitService unit tests

--- src/app/shared/services/fruit/fruit.service.spec.ts
+++ src/app/shared/services/fruit/fruit.service.spec.ts
@@ -1,149 +1,84 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpTestingController, provideHttpClientTesting, provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { FruitService } from './fruit.service';
-import { firstValueFrom } from 'rxjs';
+import { Fruit } from '@shared/types';
+import { API_BASE_PATHNAME } from '@shared/constants';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
-describe('FruitService', () => {
+fdescribe('FruitService', () => {
   let service: FruitService;
-  let httpTesting: HttpTestingController;
+  let httpMock: HttpTestingController;
+
+  const mockFruits: Fruit[] = [
+    { id: 1, name: 'Apple', genus: 'Malus', family: 'Rosaceae', order: 'Rosales', nutritions: { /* mock nutrition data */ } as Fruit['nutritions'] },
+    { id: 2, name: 'Banana', genus: 'Musa', family: 'Musaceae', order: 'Zingiberales', nutritions: { /* mock nutrition data */ } as Fruit['nutritions'] },
+  ];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-    imports: [],
-    providers: [
+      providers: [
         FruitService,
-        provideHttpClientTesting(),
+        provideExperimentalZonelessChangeDetection(),
         provideHttpClient(withInterceptorsFromDi()),
         provideHttpClientTesting(),
-    ]
-});
-    service = TestBed.inject(FruitService);
-    httpTesting = TestBed.inject(HttpTestingController);
-  });
+        { provide: API_BASE_PATHNAME, useValue: 'http://mock-api.com' }
+      ]
+    });
 
+    service = TestBed.inject(FruitService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
 
   afterEach(() => {
-    httpTesting.verify();
+    httpMock.verify();
   });
 
+  it('should retrieve all fruits', async () => {
+    const fruits$ = service.getAll();
+    const fruitsPromise = firstValueFrom(fruits$);
 
+    const req = httpMock.expectOne('http://mock-api.com/fruits/all');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockFruits);
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+    const fruits = await fruitsPromise;
+
+    expect(fruits.length).toBe(2);
+    expect(fruits[0].name).toBe('Apple');
+    expect(fruits[1].name).toBe('Banana');
   });
 
-  it('should have a base URL', () => {
-    expect(service['_baseUrl']).toBeTruthy();
+  it('should retrieve a fruit by ID', async () => {
+    const mockFruit = mockFruits[0];
+
+    const fruit$ = service.getById(1);
+    const fruitPromise = firstValueFrom(fruit$);
+
+    const req = httpMock.expectOne('http://mock-api.com/fruits/1');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockFruit);
+
+    const fruit = await fruitPromise;
+
+    expect(fruit).toEqual(mockFruit);
   });
 
+  it('should retrieve fruits with query parameters', async () => {
+    const query = { genus: 'Malus' };
 
+    const fruits$ = service.getWithQuery(query);
 
-  describe('_composeUrl', () => {
+    const fruitsPromise = firstValueFrom(fruits$);
 
-    it('should exists', () => {
-      expect(service['_composeUrl']).toBeTruthy();
-    });
+    const req = httpMock.expectOne('http://mock-api.com/fruits/genus/Malus');
+    expect(req.request.method).toBe('GET');
+    req.flush([mockFruits[0]]);
 
-    it('should return the base URL when the key is not included in the keyList', () => {
-      const key = 'key';
-      const url = service['_composeUrl'](key);
-      const expectedUrl = service['_baseUrl'];
+    const fruits = await fruitsPromise;
 
-      expect(url).toEqual(expectedUrl);
-    });
-
-    it('should return the base URL with the key appended when the key is included in the keyList', () => {
-      const key = 'all';
-      const url = service['_composeUrl'](key);
-      const expectedUrl = `${service['_baseUrl']}/${key}`;
-
-      expect(url).toEqual(expectedUrl);
-    });
-
-  });
-
-
-
-  describe('_getAll', () => {
-
-    it('should exists', () => {
-      expect(service.getAll).toBeTruthy();
-    });
-
-    it('should make a GET request to the correct URL', () => {
-      const expectedUrl = service['_composeUrl']('all');
-
-      const fruits$ = service.getAll();
-      const fruitsPromise = firstValueFrom(fruits$);
-
-      const req = httpTesting.expectOne(expectedUrl);
-      expect(req.request.method).toEqual('GET');
-
-      req.flush([]);
-    });
-    
-  });
-
-
-
-  describe('_getById', () => {
-
-    it('should exists', () => {
-      expect(service.getById).toBeTruthy();
-    });
-
-    it('should make a GET request to the correct URL', () => {
-      const id = 1;
-      const expectedUrl = `${service['_composeUrl']()}/${id}`;
-
-      const fruit$ = service.getById(id);
-      const fruitPromise = firstValueFrom(fruit$);
-
-      const req = httpTesting.expectOne(expectedUrl);
-      expect(req.request.method).toEqual('GET');
-
-      req.flush({});
-    });
-    
-  });
-
-
-
-  describe('_getWithQuery', () => {
-
-    it('should exists', () => {
-      expect(service.getWithQuery).toBeTruthy();
-    });
-
-    it('should make a single GET request to the correct URL', () => {
-      const query = { name: 'banana' };
-      const expectedUrl = `${service['_composeUrl']('key')}/${query.name}`;
-
-      const fruits$ = service.getWithQuery(query);
-      const fruitsPromise = firstValueFrom(fruits$);
-
-      const req = httpTesting.expectOne(expectedUrl);
-      expect(req.request.method).toEqual('GET');
-
-      req.flush([]);
-    });
-
-    it('should make multiple GET requests to the correct URLs', () => {
-      const query = { family: 'Musaceae', genus: 'Musa' };
-      const expectedUrl1 = `${service['_composeUrl']('family')}/${query.family}`;
-      const expectedUrl2 = `${service['_composeUrl']('genus')}/${query.genus}`;
-
-      service.getWithQuery(query).subscribe();
-
-      const req1 = httpTesting.expectOne(expectedUrl1);
-      const req2 = httpTesting.expectOne(expectedUrl2);
-      expect(req1.request.method).toEqual('GET');
-      expect(req2.request.method).toEqual('GET');
-
-      req1.flush([]);
-      req2.flush([]);
-    });
-    
+    expect(fruits.length).toBe(1);
+    expect(fruits[0].name).toBe('Apple');
   });
 });
