cf43901357847d89d32738c8a6fc7cf558e8bbd5
mancarius <mattia.mancarella.dev@gmail.com>
2024-10-08 22:20:53 +0200

    Refactor FruitPreviewPlaceholderComponent integration tests and update change detection strategy

--- src/app/shared/components/fruit-preview-placeholder/fruit-preview-placeholder.component.spec.ts
+++ src/app/shared/components/fruit-preview-placeholder/fruit-preview-placeholder.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FruitPreviewPlaceholderComponent } from './fruit-preview-placeholder.component';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 
 describe('FruitPreviewPlaceholderComponent', () => {
   let component: FruitPreviewPlaceholderComponent;
@@ -8,10 +9,10 @@ describe('FruitPreviewPlaceholderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FruitPreviewPlaceholderComponent]
+      providers: [provideExperimentalZonelessChangeDetection()],
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(FruitPreviewPlaceholderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +20,10 @@ describe('FruitPreviewPlaceholderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a spinner with a diameter of 30', () => {
+    const spinner = fixture.nativeElement.querySelector('mat-spinner');
+    expect(spinner.getAttribute('diameter')).toBe('30');
   });
 });
