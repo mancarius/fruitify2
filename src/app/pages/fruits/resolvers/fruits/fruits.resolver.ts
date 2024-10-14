ee3ede3deb38d2db92ea4db56c8c5faaefbfa3ed
mancarius <mattia.mancarella.dev@gmail.com>
2024-10-14 23:38:25 +0200

    Refactor fruitResolver to handle error redirection

--- src/app/pages/fruits/resolvers/fruits/fruits.resolver.ts
+++ src/app/pages/fruits/resolvers/fruits/fruits.resolver.ts
@@ -1,5 +1,5 @@
 import { inject } from '@angular/core';
-import { Params, ResolveFn } from '@angular/router';
+import { Params, RedirectCommand, ResolveFn, Router } from '@angular/router';
 import { FruitService } from '@shared/services/fruit/fruit.service';
 import { catchError, defer, finalize, of } from 'rxjs';
 import { Fruit, QueryParams } from '@shared/types';
@@ -9,6 +9,7 @@ export const fruitsResolver: ResolveFn<Fruit[]> = (route, state) => {
   const service = inject(FruitService);
   const loader = inject(LoadingService);
   const query = route.queryParams as Params & QueryParams<keyof Omit<Fruit, 'nutritions' | 'id'>>;
+  const router = inject(Router);
 
   loader.start();
 
@@ -17,6 +18,12 @@ export const fruitsResolver: ResolveFn<Fruit[]> = (route, state) => {
     : service.getAll()
   ).pipe(
     finalize(() => loader.stop()),
-    catchError(() => of([]))
+    catchError((err: any) => {
+      console.error('Failed to retrieve fruits', err);
+      const errorPath = router.parseUrl('/error');
+      return of(new RedirectCommand(errorPath, {
+        skipLocationChange: true,
+      }));
+    })
   );
 };
