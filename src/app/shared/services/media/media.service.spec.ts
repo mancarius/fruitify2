840f2490801309d353696af677c65cb60c613241
mancarius <mattia.mancarella.dev@gmail.com>
2024-10-20 21:56:15 +0200

    Create MediaService tests

--- src/app/shared/services/media/media.service.spec.ts
+++ src/app/shared/services/media/media.service.spec.ts
@@ -1,18 +1,70 @@
 import { TestBed } from '@angular/core/testing';
 import { MediaService } from './media.service';
 import { MEDIA_SERVICE_CONFIG_TOKEN } from '@tokens';
+import { provideExperimentalZonelessChangeDetection, signal } from '@angular/core';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { firstValueFrom, of } from 'rxjs';
+import { MediaPhoto, MediaProvidersEnum, MediaServiceConfig } from '../../types';
+import { PexelsService } from '../pexels/pexels.service';
+import { UnsplashService } from '../unsplash/unsplash.service';
 
-describe('MediaService', () => {
+
+fdescribe('MediaService', () => {
   let service: MediaService;
+  let pexelsService: PexelsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [MEDIA_SERVICE_CONFIG_TOKEN],
+      providers: [
+        provideExperimentalZonelessChangeDetection(),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+        {
+          provide: MEDIA_SERVICE_CONFIG_TOKEN,
+          useValue: signal<MediaServiceConfig>({
+            provider: MediaProvidersEnum.PEXELS,
+            authConfigs: [],
+            baseUrl: 'https://api.pexels.com/v1',
+          }),
+        }],
     });
     service = TestBed.inject(MediaService);
+    pexelsService = TestBed.inject(PexelsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should assign the correct provider based on the configuration', () => {
+    expect(service['_provider']).toBeInstanceOf(PexelsService);
+  });
+
+  it('should throw an error if media provider is not assigned', () => {
+    service['_provider'] = null;
+    expect(() => service.findPhoto('test')).toThrowError('Media provider not assigned.');
+  });
+
+  it('should call findPhoto on the assigned provider', async () => {
+    const findPhotoSpy = spyOn<any>(service['_provider'], 'findPhoto').and.returnValue(of({} as MediaPhoto));
+    await firstValueFrom(service.findPhoto('test'));
+    expect(findPhotoSpy).toHaveBeenCalledWith('test fruit', undefined);
+  });
+
+  it('should throw an error if media provider configuration is not found', () => {
+    expect(() => service['_assignProvider'](null)).toThrowError('Media provider configuration not found.');
+  });
+
+  it('should create a new provider if it does not exist in the provider map', () => {
+    service['_providerMap'].clear();
+    service['_assignProvider']({ provider: MediaProvidersEnum.UNSPLASH } as MediaServiceConfig);
+    expect(service['_provider']).toBeInstanceOf(UnsplashService);
+  });
+
+  it('should not create a new provider if it already exists in the provider map', () => {
+    service['_providerMap'].set(MediaProvidersEnum.PEXELS, pexelsService);
+    service['_assignProvider']({ provider: MediaProvidersEnum.PEXELS } as MediaServiceConfig);
+    expect(service['_provider']).toBe(pexelsService);
   });
 });
