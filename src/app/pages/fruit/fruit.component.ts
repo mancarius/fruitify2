feaaa91dfd3c34485e51554a675ca846e4cbd2d9
mancarius <mattia.mancarella.dev@gmail.com>
2024-11-16 20:19:34 +0100

    refactor(fruit-component): Simplify imports and update style binding for background image

--- src/app/pages/fruit/fruit.component.ts
+++ src/app/pages/fruit/fruit.component.ts
@@ -1,7 +1,5 @@
 import { ChangeDetectionStrategy, Component, computed, effect, inject, input } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Fruit, MediaPhoto, Nullable } from '@shared/types';
-import { FruitPreviewComponent } from '@shared/components/fruit-preview/fruit-preview.component';
 import { FruitDetailComponent } from './components/fruit-detail/fruit-detail.component';
 import { RelatedFruitsComponent, RelatedFruitsContentDirective } from '@shared/components/related-fruits/related-fruits.component';
 import { FruitListComponent } from '@shared/components/fruit-list/fruit-list.component';
@@ -12,15 +10,21 @@ import { MAX_SUGGESTIONS_PREVIEW_OPTION } from '@tokens';
   selector: 'app-fruit',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, FruitPreviewComponent, FruitDetailComponent, RelatedFruitsComponent, RelatedFruitsContentDirective, FruitListComponent, MatCardModule],
+  imports: [
+    FruitDetailComponent,
+    RelatedFruitsComponent,
+    RelatedFruitsContentDirective,
+    FruitListComponent,
+    MatCardModule
+  ],
   host: {
     class: 'w-full min-h-screen flex flex-col justify-center items-center relative'
   },
   template: `
     <article class="w-full dark:text-slate-200 flex flex-col gap-4 grow">
       <header class="relative w-full h-64 sm:h-96 flex flex-col justify-end items-center bg-center bg-no-repeat bg-cover"
-        [ngStyle]="{'background-image': heroBackgroundImage()}">
-        <span id="blackOverlay" class="w-full h-full absolute opacity-60 bg-black z-0 dark:bg-black bg-white"></span>
+        [style.background-image]="heroBackgroundImage()">
+        <span id="blackOverlay" class="w-full h-full absolute opacity-60 z-0 dark:bg-black bg-white"></span>
         <h2
           class="text-4xl sm:text-6xl font-bold text-black dark:text-white z-10 pl-4 md:pl-0 pb-4 md:pb-2 w-full max-w-screen-sm">
           {{fruit()?.name}}
@@ -28,7 +32,7 @@ import { MAX_SUGGESTIONS_PREVIEW_OPTION } from '@tokens';
       </header>
 
       <main class="flex flex-col items-center p-4 z-1 z-10 w-full">
-        <app-fruit-detail class="block w-full max-w-screen-sm flex flex-col gap-4" [fruit]="fruit()"></app-fruit-detail>
+        <app-fruit-detail class="w-full max-w-screen-sm flex flex-col gap-4" [fruit]="fruit()"></app-fruit-detail>
       </main>
     </article>
 
@@ -36,7 +40,7 @@ import { MAX_SUGGESTIONS_PREVIEW_OPTION } from '@tokens';
       <aside class="w-full flex flex-col gap-4 p-4 pt-0">
         <h3 class="font-bold text-sm text-black dark:text-white m-0 w-full max-w-screen-sm mx-auto">Related fruits</h3>
 
-        <mat-card class="w-full bg-white dark:bg-slate-50/10 mx-4 w-full max-w-screen-sm mx-auto">
+        <mat-card class="w-full bg-white dark:bg-slate-50/10 max-w-screen-sm mx-auto">
           <mat-card-content>
             @defer (on viewport) {
               <nav class="w-full">
@@ -58,7 +62,7 @@ import { MAX_SUGGESTIONS_PREVIEW_OPTION } from '@tokens';
   `,
 })
 export class FruitComponent {
-  maxSuggestions = inject(MAX_SUGGESTIONS_PREVIEW_OPTION);
+  readonly maxSuggestions = inject(MAX_SUGGESTIONS_PREVIEW_OPTION);
 
   readonly photo = input.required<Nullable<MediaPhoto>>();
 
