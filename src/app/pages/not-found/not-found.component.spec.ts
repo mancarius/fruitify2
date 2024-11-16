ed762f6384c131daf4e1f2cbc8973d81d148ab92
mancarius <mattia.mancarella.dev@gmail.com>
2024-11-16 12:24:46 +0100

    test(not-found): Enhance NotFoundComponent test suite with additional assertions and zoneless change detection

--- src/app/pages/not-found/not-found.component.spec.ts
+++ src/app/pages/not-found/not-found.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NotFoundComponent } from './not-found.component';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('NotFoundComponent', () => {
   let component: NotFoundComponent;
@@ -8,16 +10,37 @@ describe('NotFoundComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NotFoundComponent]
+      imports: [NotFoundComponent],
+      providers: [
+        provideExperimentalZonelessChangeDetection(),
+      ],
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(NotFoundComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component)
+      .withContext('Expected component instance to be truthy')
+      .toBeTruthy();
+  });
+
+  it('should display the correct title', () => {
+    const titleElement: HTMLTitleElement = fixture.debugElement.query(By.css('h1')).nativeElement;
+    expect(titleElement.textContent).toBe('404 - Not Found');
+  });
+
+  it('should display the correct paragraph text', () => {
+    const paragraphElement: HTMLParagraphElement = fixture.debugElement.query(By.css('p')).nativeElement;
+    expect(paragraphElement.textContent).toContain('It seems like you are lost. Please,');
+  });
+
+  it('should have a link that navigates to the home page', () => {
+    const linkElement: HTMLAnchorElement = fixture.debugElement.query(By.css('a')).nativeElement;
+    expect(linkElement.getAttribute('href')).toBe('/');
+    expect(linkElement.textContent).toBe('go back to the safe zone');
   });
 });
