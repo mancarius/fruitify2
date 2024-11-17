13c4e05b16a39edaef1ef91d0fa38c4c6d7d563a
mancarius <mattia.mancarella.dev@gmail.com>
2024-11-17 16:49:54 +0100

    refactor(calculate-recommended-daily-intake-percentage): Enhance type safety and ensure numeric calculations

--- src/app/pages/fruit/components/fruit-nutrition-view/helpers/calculate-recommended-daily-intake-percentage.ts
+++ src/app/pages/fruit/components/fruit-nutrition-view/helpers/calculate-recommended-daily-intake-percentage.ts
@@ -2,23 +2,23 @@ import { Nutritions } from '@shared/types';
 
 /**
  * Calculates the recommended daily intake percentage for a given nutrition value.
- * 
+ *
  * @param value - The value of the nutrition.
  * @param nutritionType - The type of nutrition (carbohydrates, protein, fat, calories, sugar).
  * @returns The recommended daily intake percentage.
  */
-export function calculateRecommendedDailyIntakePercentage(value: number, nutritionType: keyof Nutritions): number {
+export function calculateRecommendedDailyIntakePercentage<T extends Nutritions>(value: T[keyof T], nutritionType: keyof T): number {
   switch (nutritionType) {
     case 'carbohydrates':
-      return value / 50 * 100;
+      return Number(value) / 50 * 100;
     case 'protein':
-      return value / 25 * 100;
+      return Number(value) / 25 * 100;
     case 'fat':
-      return value / 20 * 100;
+      return Number(value) / 20 * 100;
     case 'calories':
-      return value / 200 * 100;
+      return Number(value) / 200 * 100;
     case 'sugar':
-      return value / 5 * 100;
+      return Number(value) / 5 * 100;
     default:
       return 0;
   }
