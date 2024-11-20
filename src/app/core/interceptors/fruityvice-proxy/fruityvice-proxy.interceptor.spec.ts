9cf0ea906a91a3a1d475b68b934eb80a1f2aaf5d
mancarius <mattia.mancarella.dev@gmail.com>
2024-11-20 22:31:06 +0100

    test(fruityvice-proxy.interceptor): Enhance tests with zoneless change detection and update base URL handling

--- src/app/core/interceptors/fruityvice-proxy/fruityvice-proxy.interceptor.spec.ts
+++ src/app/core/interceptors/fruityvice-proxy/fruityvice-proxy.interceptor.spec.ts
@@ -1,24 +1,34 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClient, HttpInterceptorFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpClient, HttpInterceptorFn, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { fruityviceProxyInterceptor } from './fruityvice-proxy.interceptor';
 import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { FRUITYVICE_PROXY_BASE_URL } from '@shared/constants';
+import { environment } from '@env/environment';
 
 describe('fruityviceProxyInterceptor', () => {
   let httpTesting: HttpTestingController;
   let httpClient: HttpClient;
-  const interceptor: HttpInterceptorFn = (req, next) => 
+  const interceptor: HttpInterceptorFn = (req, next) =>
     TestBed.runInInjectionContext(() => fruityviceProxyInterceptor(req, next));
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
+        provideExperimentalZonelessChangeDetection(),
         provideHttpClient(withInterceptors([fruityviceProxyInterceptor])),
         provideHttpClientTesting(),
+        {
+          provide: FRUITYVICE_PROXY_BASE_URL,
+          useValue: 'https://example.com/api'
+        }
       ]
     });
 
     httpTesting = TestBed.inject(HttpTestingController);
     httpClient = TestBed.inject(HttpClient);
+    environment.production = true;
   });
 
   afterEach(() => {
@@ -30,12 +40,16 @@ describe('fruityviceProxyInterceptor', () => {
   });
 
   it('should replace the base URL with the Fruityvice API url', async () => {
-    const req = new HttpRequest('GET', 'http://localhost:4200/api/fruit/all');
-    const expected = 'https://fruityvice.com/api/fruit/all';
+    const url = 'http://localhost:4200/api/fruit/all';
+    const expected = 'https://example.com/api/fruit/all';
 
-    httpClient.get(req.url).subscribe();
+    const get$ = httpClient.get(url);
+    const getPromise = firstValueFrom(get$);
 
-    httpTesting.expectOne(expected);
+    const req = httpTesting.expectOne(expected);
+    req.flush(null);
+
+    await getPromise;
 
     expect().nothing();
   });
