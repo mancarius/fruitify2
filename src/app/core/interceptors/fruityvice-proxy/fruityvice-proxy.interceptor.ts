c971c1b33c19682dfa87dafe30aa46deab791ea4
mancarius <mattia.mancarella.dev@gmail.com>
2024-11-20 22:30:54 +0100

    feat(fruityvice-proxy.interceptor): Enhance proxy URL modification and error handling in interceptor

--- src/app/core/interceptors/fruityvice-proxy/fruityvice-proxy.interceptor.ts
+++ src/app/core/interceptors/fruityvice-proxy/fruityvice-proxy.interceptor.ts
@@ -4,6 +4,25 @@ import { environment } from '@env/environment';
 import { API_BASE_PATHNAME, FRUITYVICE_PROXY_BASE_URL } from '@shared/constants';
 
 /**
+ * Modifies the given request URL to use the hostname, protocol, and port of the proxy base URL.
+ *
+ * @param reqUrl - The original request URL to be modified.
+ * @param proxyBaseUrl - The base URL of the proxy server.
+ * @returns The modified URL as a string.
+ */
+const modifyUrlForProxy = (reqUrl: string, proxyBaseUrl: string): string => {
+  const proxyUrl = new URL(proxyBaseUrl);
+  const url = new URL(reqUrl);
+
+  url.hostname = proxyUrl.hostname;
+  url.protocol = proxyUrl.protocol;
+  url.port = proxyUrl.port;
+
+  return url.toString();
+};
+
+
+/**
  * Interceptor function that modifies the request URL to use a proxy server for requests matching the base URL.
  * @param req - The original HTTP request.
  * @param next - The next interceptor in the chain.
@@ -15,21 +34,22 @@ export const fruityviceProxyInterceptor: HttpInterceptorFn = (req, next) => {
     return next(req);
   }
 
-  const apiPathname = inject(API_BASE_PATHNAME);
-  const matchesBaseUrl = req.url.includes(apiPathname);
+  try {
+    const apiPathname = inject(API_BASE_PATHNAME);
+    const matchesBaseUrl = req.url.includes(apiPathname);
 
-  if (!matchesBaseUrl) {
+    if (!matchesBaseUrl) {
+      return next(req);
+    }
+
+    const proxyBaseUrl = inject(FRUITYVICE_PROXY_BASE_URL);
+    const modifiedUrl = modifyUrlForProxy(req.url, proxyBaseUrl);
+
+    req = req.clone({ url: modifiedUrl });
+  } catch (error) {
+    console.error('Error in fruityviceProxyInterceptor:', error);
     return next(req);
   }
-
-  const url = new URL(inject(FRUITYVICE_PROXY_BASE_URL));
-  url.pathname = apiPathname;
-  url.protocol = 'https';
-  url.port = '';
-
-  req = req.clone({
-    url: url.toString()
-  });
 
   return next(req);
 };
