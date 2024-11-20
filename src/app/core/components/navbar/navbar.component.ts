864240e983f00e13799e28ef7bb2936957b8dcd5
mancarius <mattia.mancarella.dev@gmail.com>
2024-11-20 22:57:14 +0100

    feat(navbar.component): remove svg icons registration from inside the component

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -10,15 +9,14 @@ import { MatRippleModule } from '@angular/material/core';
 @Component({
   selector: 'app-navbar',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
-    CommonModule,
     ThemeTogglerComponent,
     MatButtonModule,
     MatIconModule,
     RouterLink,
     MatRippleModule,
   ],
-
   template: `
     <nav class="w-full flex flex-wrap items-center justify-between px-2 py-3">
       <div
@@ -26,7 +24,7 @@ import { MatRippleModule } from '@angular/material/core';
       >
         <div class="relative flex lg:w-auto lg:static lg:block">
           <a
-            class="text-sm font-bold leading-relaxed inline-block mr-4 ml-2 py-2 whitespace-nowrap uppercase flex items-center"
+            class="text-sm font-bold leading-relaxed mr-4 ml-2 py-2 whitespace-nowrap uppercase flex items-center"
             routerLink="/"
           >
             Fruitify
@@ -40,16 +38,16 @@ import { MatRippleModule } from '@angular/material/core';
             class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block md:hidden outline-none focus:outline-none text-white"
             type="button"
             (click)="toggleNavbar()"
+            data-testid="toggle-navbar-btn"
           >
-            <mat-icon class="opacity-80 text-gray-900 dark:text-gray-100 ">{{
-              showMenu ? 'menu_open' : 'menu'
-            }}</mat-icon>
+            @let icon = showMenu() ? 'menu_open' : 'menu';
+            <mat-icon class="opacity-80 text-gray-900 dark:text-gray-100 ">{{icon}}</mat-icon>
           </button>
         </div>
 
         <div
           class="absolute md:relative top-full left-0 w-full md:w-auto md:flex items-stretch bg-white dark:bg-gray-900 md:bg-transparent md:dark:bg-transparent md:shadow-none"
-          [ngClass]="{ hidden: !showMenu, block: showMenu }"
+          [class.hidden]="!showMenu()" [class.block]="showMenu()"
         >
           <ul class="flex flex-col md:flex-row md:items-stretch list-none md:ml-auto">
             <li class="flex items-stretch">
@@ -80,22 +78,10 @@ import { MatRippleModule } from '@angular/material/core';
   `,
 })
 export class NavbarComponent {
-  #matIconRegistry = inject(MatIconRegistry);
-  #domSanitizer = inject(DomSanitizer);
-
-  protected showMenu = false;
-  protected ghLink = 'https://github.com/mancarius/fruitify2';
-
-  constructor() {
-    this.#matIconRegistry.addSvgIcon(
-      'github',
-      this.#domSanitizer.bypassSecurityTrustResourceUrl(
-        'assets/icons/github.svg'
-      )
-    );
-  }
+  readonly showMenu = signal(false);
+  protected readonly ghLink = 'https://github.com/mancarius/fruitify2';
 
   toggleNavbar() {
-    this.showMenu = !this.showMenu;
+    this.showMenu.update(v => !v);
   }
 }
