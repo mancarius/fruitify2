36cf4de89e5e344691b53bf50e26385eaff2db25
mancarius <mattia.mancarella.dev@gmail.com>
2024-11-24 21:45:50 +0100

    feat(app): integrate ConfigsModule and ConfigModule for configuration management

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FruitsModule } from './fruits/fruits.module';
+import { ConfigsModule } from './configs/configs.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [FruitsModule],
+  imports: [FruitsModule, ConfigsModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
