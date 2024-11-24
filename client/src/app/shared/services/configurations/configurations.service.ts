c25b95a275caf7dc4468dbc738cb8f8b8cc90f4a
mancarius <mattia.mancarella.dev@gmail.com>
2024-11-24 21:47:37 +0100

    feat(configs): add EnvironmentConfigService to fetch configuration settings

---
+++ client/src/app/shared/services/configurations/configurations.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { API_BASE_PATHNAME } from "@shared/constants";
+
+export type EnvironmentConfigs = {
+  pexelsApiKey: string;
+  unsplashApiKey: string;
+};
+
+@Injectable({
+  providedIn: "root"
+})
+export class EnvironmentConfigService {
+  readonly #baseUrl = inject(API_BASE_PATHNAME);
+
+  readonly #http = inject(HttpClient);
+
+  /**
+   * Fetches the configuration settings from the server.
+   *
+   * @returns An Observable containing the configuration settings.
+   */
+  public getConfigs(env: 'prod' | 'dev' = 'dev') {
+    const url = `${this.#baseUrl}/configs`;
+    return this.#http.get<EnvironmentConfigs>(url, { params: { env } });
+  }
+}
