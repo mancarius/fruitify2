0af7aaae66c21b727765f0bcd42ae108e2abac69
mancarius <mattia.mancarella.dev@gmail.com>
2024-11-24 21:48:07 +0100

    feat(environments): add Pexels and Unsplash API keys to common environment configuration

--- client/src/environments/environment.common.ts
+++ client/src/environments/environment.common.ts
@@ -2,4 +2,8 @@ export const environment = {
   api: 'fruityvice.com',
 
   fruityviceProxy: 'http://localhost:3000',
+
+  pexelsApiKey: "PEXELS_API_KEY",
+
+  unsplashApiKey: "UNSPLASH_API_KEY",
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,7 +2,5 @@ import { environment as common } from './environment.common';
 
 export const environment = {
   ...common, // Spread the common environment properties
-  production: true,
-  pexelsApiKey: "PEXELS_API_KEY",
-  unsplashApiKey: "UNSPLASH_API_KEY",
+  production: true
 };
